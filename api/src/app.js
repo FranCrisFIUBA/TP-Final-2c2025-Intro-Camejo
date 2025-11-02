9cfa85c0e7a1f9b3a7637a289b0c2a5d3630c457
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-02 19:01:02 -0300

    Se creo el proyecto en node

---
+++ api/src/app.js
@@ -0,0 +1,13 @@
+
+import express from 'express'
+
+const APP = express()
+const PORT = 3000
+
+APP.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+APP.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
