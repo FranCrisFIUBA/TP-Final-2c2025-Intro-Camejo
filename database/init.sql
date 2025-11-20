1512270739911b15c8b165227914e0774046d606
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-19 22:57:29 -0300

    Se creo la tabla de usuarios

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,10 @@
 -- Esta query tiene como objetivo generar las tablas necesarias al iniciar la base de datos.
 
 -- TODO: tabla usuarios
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY
+);
+
 -- TODO: tabla publicaciones
 -- TODO: tabla likes usuarios a publicaciones
 -- TODO: tabla comentarios
