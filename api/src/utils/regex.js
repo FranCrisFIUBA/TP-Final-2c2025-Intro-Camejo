e53e0725db926f17caf1fa49f41dd5343ad79561
Franco Cristaldo <francocristaldo@gmail.com>
2025-11-26 16:31:02 -0300

    Se completaron los reguex de regex.js

--- api/src/utils/regex.js
+++ api/src/utils/regex.js
@@ -1,5 +1,6 @@
 
-const regexNombre = /^[A-Za-z0-9._-]{6,25}$/;
-const regexContrasenia = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[._-]).{5,25}$/
-const regexEmail = /^$/ // TODO: completar el regex para los emails
-const regexUrl = /^$/ // TODO: completar el regex para las urls
+export const regexNombre = /^[A-Za-z0-9._-]{6,25}$/;
+export const regexContrasenia = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[._-]).{5,25}$/
+export const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const regexUrl = /^[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_+.~#?&\/=]*$/;
+export const regexUrlHttp = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_+.~#?&\/=]*$/;
