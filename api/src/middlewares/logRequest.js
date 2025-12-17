32077fdb0696b315671c8e7a006aaa7186309bbf
Franco Cristaldo <francocristaldo@gmail.com>
2025-12-17 20:39:02 -0300

    Se corrigio logRequest y logResponse

--- api/src/middlewares/logRequest.js
+++ api/src/middlewares/logRequest.js
@@ -1,6 +1,6 @@
 
 export async  function logRequest(req, res, next) {
     console.debug(`Request: ${req.method} ${req.url}`)
-    console.trace(`Request Body: ${req.body}`)
+    console.trace(`Request Body: ${JSON.stringify(req.body)}`)
     next()
 }
--- api/src/middlewares/logResponse.js
+++ api/src/middlewares/logResponse.js
@@ -4,7 +4,7 @@ export async function logResponse(req, res, next) {
     res.on("finish", () => {
         const duration = Date.now() - start;
         console.debug(`Response: ${req.method} ${req.url} -> ${res.statusCode} (estimate: ${duration}ms)`);
-        console.trace(`Response Body: ${req.body}`);
+        console.trace(`Response Body: ${JSON.stringify(res.body)}`);
     })
     next()
 }
