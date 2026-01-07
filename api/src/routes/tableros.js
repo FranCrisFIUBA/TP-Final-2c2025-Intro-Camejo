6aa1a214374011eddc8127aa75a3aa118042c15c
Franco Cristaldo <francocristaldo@gmail.com>
2026-01-06 21:22:04 -0300

    Se agrego el endpoint GET tableros/:id

--- api/src/routes/tableros.js
+++ api/src/routes/tableros.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { pool } from "../db.js";
+import {intentarObtenerTableroPorId} from "../utils/database/tableros.js";
 
 const router = express.Router();
 
-
 // GET tableros/
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
         pool.query(`SELECT * FROM tableros`)
             .then(result => {
@@ -18,5 +18,23 @@ router.get('/', (req, res) => {
     } catch (e) {
         console.error(e);
         res.status(500).json({ error: "Error del servidor al obtener tableros" });
+    }
+})
+
+// GET tableros/:id
+router.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const tablero = await intentarObtenerTableroPorId(id);
+
+        if (!tablero) {
+            res.status(404).json({error: "No se pudo encontrar el tablero de Id " + id});
+        }
+
+        res.status(200).json(tablero);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({error: "Error del servidor al obtener el tablero de Id " + req.params.id});
     }
 })
