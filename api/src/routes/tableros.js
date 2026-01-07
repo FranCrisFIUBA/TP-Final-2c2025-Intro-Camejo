6e0061424ab2d771935a76a2dffbe2129cdf3ddb
Franco Cristaldo <francocristaldo@gmail.com>
2026-01-06 21:29:45 -0300

    Se agrego el endpoint DELETE tableros/:id

--- api/src/routes/tableros.js
+++ api/src/routes/tableros.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { pool } from "../db.js";
 import {intentarObtenerTableroPorId} from "../utils/database/tableros.js";
+import {existeUsuarioConId} from "../utils/database/usuarios.js";
 
 const router = express.Router();
 
@@ -36,5 +37,24 @@ router.get('/:id', async (req, res) => {
     } catch (e) {
         console.error(e);
         res.status(500).json({error: "Error del servidor al obtener el tablero de Id " + req.params.id});
+    }
+})
+
+// DELETE tableros/:id
+router.delete('/:id', async (req, res) => {
+    try {
+        const { rowCount } = await pool.query(
+            "DELETE FROM tableros WHERE id = $1 RETURNING id",
+            [req.params.id]
+        );
+
+        if (rowCount === 0) {
+            return res.status(404).json({ error: "Tablero no encontrado" });
+        }
+
+        res.status(200).json({ message: "Tablero eliminado" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Error al eliminar el tablero" });
     }
 })
