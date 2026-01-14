ec4fb81325221a5711274af5b16e339067c131d2
CatherineMerida <cmerida@fi.uba.ar>
2026-01-13 22:58:31 -0300

    se soluciono el problema de que se muestren las opciones de editar y eliminar en publicaciones que no son del usuario logueado

--- front/JS/componentes/card.js
+++ front/JS/componentes/card.js
@@ -4,15 +4,17 @@ const API_IMAGENES = API_BASE_URL + '/imagenes';
 const API_ICONOS = API_BASE_URL + '/iconos';
 
 export function crearCard(
-  card,
-  {
-    onOpenModal = () => {},
-    onGoToProfile = () => {},
-    editable = false,
-    onEdit = () => {},
-    onDelete = () => {}
-  } = {}
+    card,
+    {
+        onOpenModal = () => {},
+        onGoToProfile = () => {},
+        onEdit = () => {},
+        onDelete = () => {}
+    } = {}
 ) {
+    const usuarioData = localStorage.getItem("usuarioLogueado");
+    const usuarioLogueado = usuarioData ? JSON.parse(usuarioData) : null;
+    const esDueno = usuarioLogueado && Number(usuarioLogueado.id) === Number(card.usuario_id);
     const AVATAR_DEFAULT = './img/avatar-default.jpg';
     const cardDiv = document.createElement("div");
     cardDiv.className = "card";
@@ -48,7 +50,7 @@ export function crearCard(
             <span class="author-name">${card.usuario_nombre || ''}</span>
         </div>
     `;
-    if (editable) {
+    if (esDueno) {
         const actions = document.createElement("div");
         actions.className = "card-actions";
 
