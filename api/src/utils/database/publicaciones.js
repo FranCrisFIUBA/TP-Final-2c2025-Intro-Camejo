76d94a6c6d8fa86e12a56f025e36dfbb16025c5a
Franco Cristaldo <francocristaldo@gmail.com>
2026-01-16 18:33:28 -0300

    Se agrego el metodo getPublicacionesConBusqueda

--- api/src/utils/database/publicaciones.js
+++ api/src/utils/database/publicaciones.js
@@ -9,3 +9,48 @@ export async function intentarConseguirPublicacionPorId(id) {
 
     return esquemaPublicacion.safeParseAsync(result.rows[0])
 }
+
+export async function getPublicacionesConBusqueda(params) {
+    validarParametrosDeBusqueda(params);
+
+    const {
+        autor_id,
+        etiquetas,
+        likes_minimos, likes_maximos,
+        fecha_minima, fecha_maxima,
+        alto_minimo, alto_maximo,
+        ancho_minimo, ancho_maximo,
+    } = params;
+
+    const condiciones = [];
+    const queryParams = [];
+
+    const addCond = (sql, value) => {
+        queryParams.push(value);
+        condiciones.push(sql.replace("?", `$${queryParams.length}`));
+    };
+
+    if (autor_id !== undefined)         addCond("usuario_id = ?", autor_id);
+    if (likes_minimos !== undefined)    addCond("likes >= ?", likes_minimos);
+    if (likes_maximos !== undefined)    addCond("likes <= ?", likes_maximos);
+    if (fecha_minima !== undefined)     addCond("fecha_publicacion >= ?", fecha_minima);
+    if (fecha_maxima !== undefined)     addCond("fecha_publicacion <= ?", fecha_maxima);
+    if (alto_minimo !== undefined)      addCond("alto >= ?", alto_minimo);
+    if (alto_maximo !== undefined)      addCond("alto <= ?", alto_maximo);
+    if (ancho_minimo !== undefined)     addCond("ancho >= ?", ancho_minimo);
+    if (ancho_maximo !== undefined)     addCond("ancho <= ?", ancho_maximo);
+
+    // TODO: implementar busqueda por etiquetas
+
+    const query =
+        condiciones.length === 0
+            ?`SELECT * FROM publicaciones`
+            :`SELECT * FROM publicaciones WHERE ${condiciones.join(" AND ")}`
+    ;
+
+    return pool.query(query, queryParams);
+}
+
+function validarParametrosDeBusqueda(params) {
+    return true;
+}
