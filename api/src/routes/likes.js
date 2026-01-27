96fc85a03956b736454169663b1c52ff183419d9
fran <francocristaldo2004@gmail.com>
2026-01-27 18:20:08 -0300

    Se implemento GET /likes/publicacion/:publicacion_id

--- api/src/routes/likes.js
+++ api/src/routes/likes.js
@@ -37,4 +37,22 @@ likes.get('/likes/usuario/:usuario_id', async (req, res) => {
     }
 })
 
+
+// GET /likes/publicacion/publicacion_id
+likes.get('/likes/publicacion/:publicacion_id', async (req, res) => {
+    try {
+        const { publicacion_id } = req.params;
+        const { count_only } = req.query;
+
+        const result = count_only === true
+            ? await pool.query('SELECT * FROM likes WHERE publicacion_id = ?', [publicacion_id])
+            : await pool.query('SELECT COUNT(*) FROM likes  WHERE publicacion_id = ?', [publicacion_id]);
+
+        res.status(200).json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Error del servidor al obtener los likes de la publicacion" });
+    }
+})
+
 export default likes
