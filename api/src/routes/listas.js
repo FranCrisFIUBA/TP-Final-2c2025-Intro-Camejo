3d239dc20a28ee696b01e8651a6723b361c35b7c
Franco Cristaldo <francocristaldo@gmail.com>
2026-02-04 17:40:41 -0300

    Se implemento POST /listas/

--- api/src/routes/listas.js
+++ api/src/routes/listas.js
@@ -75,7 +75,67 @@ listas.get("/:id/publicaciones", async (req, res) => {
 // POST /
 // Crea una nueva lista
 listas.post("/", async (req, res) => {
+    try {
+        const {
+            usuario_id,
+            titulo,
+            etiquetas,
+            likes_minimos,
+            likes_maximos,
+            fecha_minima,
+            fecha_maxima,
+            alto_minimo,
+            alto_maximo,
+            ancho_minimo,
+            ancho_maximo
+        } = req.body;
 
+        if (!usuario_id || !titulo)
+            return res.status(400).json({ error: "usuario_id y titulo son obligatorios" });
+
+        if (likes_minimos !== null && likes_maximos !== null && likes_minimos > likes_maximos)
+            return res.status(400).json({ error: "likes_minimos > likes_maximos" });
+
+        if (alto_minimo !== null && alto_maximo !== null && alto_minimo > alto_maximo)
+            return res.status(400).json({ error: "alto_minimo > alto_maximo" });
+
+        if (ancho_minimo !== null && ancho_maximo !== null && ancho_minimo > ancho_maximo)
+            return res.status(400).json({ error: "ancho_minimo > ancho_maximo" });
+
+        if (fecha_minima && fecha_maxima && new Date(fecha_minima) > new Date(fecha_maxima))
+            return res.status(400).json({ error: "fecha_minima > fecha_maxima" });
+
+        const result = await pool.query(`
+            INSERT INTO listas (usuario_id,
+                                titulo,
+                                etiquetas,
+                                likes_minimos,
+                                likes_maximos,
+                                fecha_minima,
+                                fecha_maxima,
+                                alto_minimo,
+                                alto_maximo,
+                                ancho_minimo,
+                                ancho_maximo)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+            RETURNING *;
+        `, [usuario_id,
+            titulo,
+            etiquetas,
+            likes_minimos,
+            likes_maximos,
+            fecha_minima,
+            fecha_maxima,
+            alto_minimo,
+            alto_maximo,
+            ancho_minimo,
+            ancho_maximo]);
+
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error al crear la lista" });
+    }
 })
 
 // DELETE /:id
