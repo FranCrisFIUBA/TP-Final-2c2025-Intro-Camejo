3bec8bcfd1fba69408172f9607422f05c0760fa5
Franco Cristaldo <francocristaldo@gmail.com>
2026-02-04 20:17:44 -0300

    Se configuro front para poder cargar las variables de entorno

--- front/JS/api.js
+++ front/JS/api.js
@@ -1,5 +1,6 @@
 
-export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = window.API_URL || "http://localhost:3000";
+
 export const API_PUBLICACIONES_URL = API_BASE_URL + "/publicaciones";
 export const API_IMAGENES_URL = API_BASE_URL + '/imagenes';
 export const API_ICONOS_URL = API_BASE_URL + '/iconos';
