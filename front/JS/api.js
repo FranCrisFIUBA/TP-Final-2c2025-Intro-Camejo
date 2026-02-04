222930e595ccf16477559db7a7346ba73f821d77
Franco Cristaldo <francocristaldo@gmail.com>
2026-02-04 20:30:55 -0300

    Se agrego la constante API_LIKES_URL

--- front/JS/api.js
+++ front/JS/api.js
@@ -6,3 +6,4 @@ export const API_IMAGENES_URL = API_BASE_URL + '/imagenes';
 export const API_ICONOS_URL = API_BASE_URL + '/iconos';
 export const API_USUARIOS_URL = API_BASE_URL + "/usuarios";
 export const API_TABLEROS_URL = API_BASE_URL + "/tableros";
+export const API_LIKES_URL = API_BASE_URL + "/likes";
