80ed1b4fc20c9caff976ac00dfb69b122b016e23
Franco Cristaldo <francocristaldo@gmail.com>
2026-02-04 17:43:34 -0300

    Se implemento DELETE /listas/:id

--- api/src/routes/listas.js
+++ api/src/routes/listas.js
@@ -141,7 +141,24 @@ listas.post("/", async (req, res) => {
 // DELETE /:id
 // Elimina una lista por id
 listas.delete("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
 
+        const result = await pool.query(`
+            DELETE FROM listas
+            WHERE id=$1
+            RETURNING *;
+        `, [id]);
+
+        if (result.rows.length === 0) {
+            res.status(404).json({ error: "No se ha encontrado la lista" });
+        }
+
+        res.status(200).send(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error al eliminar la lista" });
+    }
 })
 
 // PATCH /:id
