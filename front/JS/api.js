69a19706dfed3fdde0067016e1e6b4b63833b3bb
Franco Cristaldo <francocristaldo@gmail.com>
2026-02-04 20:33:58 -0300

    Se agrego la constante API_COMENTARIOS_URL

--- front/JS/api.js
+++ front/JS/api.js
@@ -7,3 +7,4 @@ export const API_ICONOS_URL = API_BASE_URL + '/iconos';
 export const API_USUARIOS_URL = API_BASE_URL + "/usuarios";
 export const API_TABLEROS_URL = API_BASE_URL + "/tableros";
 export const API_LIKES_URL = API_BASE_URL + "/likes";
+export const API_COMENTARIOS_URL = API_BASE_URL + "/commentarios";
